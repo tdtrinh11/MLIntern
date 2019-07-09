76dc510ce6f0d7bde9299591f86ec62af22fb147
tdtrinh11 <tdtrinh11@gmail.com>
2019-07-09 14:59:51 +0700

    change name of folder

--- Project/ProcessData.py
+++ Project/ProcessData.py
@@ -3,8 +3,7 @@ from collections import defaultdict
 import csv
 
 # episodes = defaultdict(list)
-with open("/home/tdtrinh11/Documents/sentences.csv", "r") as sentences_file:
-    reader = csv.reader(sentences_file, delimiter = ',')
-    line_count = 0
-    for row in reader:
-        print(row)
+# with open("./sentences.csv", "r") as sentences_file:
+#     reader = csv.reader(sentences_file, delimiter = ',')
+#     line_count = 0
+#     print(type(reader))
