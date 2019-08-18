0e77621e01bf8057b6dafd04e61d1a73548a523d
tdtrinh11 <tdtrinh11@gmail.com>
2019-08-18 20:23:53 +0700

    .

--- exercise-of-machine-learning-notebooks/ex1.py
+++ exercise-of-machine-learning-notebooks/ex1.py
@@ -2,23 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # doc du lieu tu file
-f = open('ex1data1.txt', 'r')
+f = open('../Data/ex1data1.txt', 'r')
 x = f.readline()
 X = []
 y = []
 while x != "":
     z = x.split(',')
-    for i in range(len(z)):
-        z[i] = z[i].split()
-    # print(y[1][0])
-    X.append(float(z[0][0]))
-    y.append(float(z[1][0]))
-    # print(y)
+    X.append(float(z[0]))
+    y.append(float(z[1]))
+
     x = f.readline()
 
 X = np.array([X]).T
 y = np.array([y]).T
-# print(X)
+print(X)
 
 # X la du lieu cot dau tien
 # y la du lieu cot thu 2
@@ -26,12 +23,12 @@ y = np.array([y]).T
 # thuat toan hoi quy
 # tao cot 1
 one = np.ones((X.shape[0], 1))
-# print(one)
+print(len(one))
 # X.shape[0] tra ve so hang cua ma tran X
 
 # ma tran dung de tinh toan
 Xbar = np.concatenate((one, X), axis = 1)
-# print(Xbar)
+# print(len(Xbar))
 
 # tinh toan
 A = np.dot(Xbar.T, Xbar)
@@ -40,15 +37,16 @@ w = np.dot(np.linalg.pinv(A), b)
 # print('w = ', w)
 w_0 = w[0][0]
 w_1 = w[1][0]
-x0 = np.linspace(5, 15)
+x0 = np.linspace(5, 25)
 y0 = w_0 + w_1 * x0
 # print('x0 =', x0)
+print(type(x0))
 # print(y0)
 
 # ve do thi
 plt.plot(X.T, y.T, 'ro')
 plt.plot(x0, y0)
-plt.axis([5, 15, -5, 20])
+plt.axis([4, 25, -5, 25])
 plt.xlabel("abc")
 plt.ylabel("xyz")
-plt.show()
+# plt.show()
--- exercise-of-machine-learning-notebooks/ex1_data2.py
+++ exercise-of-machine-learning-notebooks/ex1_data2.py
@@ -2,16 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # doc du lieu tu file
-f = open('ex1data2.txt', 'r')
+f = open('../Data/ex1data2.txt', 'r')
 x = f.readline()
-X = [[]]
+X1 = []
+X2 = []
 y = []
 while x != "":
     z = x.split(',')
-    for i in range(len(z)):
-        z[i] = z[i].split()
-    # print(y[1][0])
-    X.append(float(z[0][0]))
-    y.append(float(z[1][0]))
-    # print(y)
+    X1.append(float(z[0]))
+    X2.append(float(z[1]))
+    y.append(float(z[2]))
+    
     x = f.readline()
+
+# print(len(X1), len(X2), len(y))
+# print(np.array([X1]))
+one = np.ones((len(X1), 1))
+# print(one)
+X = np.concatenate((one, np.array([X1]).T, np.array([X2]).T), axis=1)
+# print(X)
+y = np.array([y])
